a7251f41849ce115c0ba8ddc46bdc40614febf33
Nikita <krasnovnikita1991@gmail.com>
2017-07-25 00:32:00 +0300

    adding index to missing files in git and commit

---
+++ JavaBlogAggregator/src/main/java/ua/krasnovnikita/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ua.krasnovnikita.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+
+}
