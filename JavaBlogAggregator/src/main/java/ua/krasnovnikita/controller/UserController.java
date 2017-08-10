0c2412120ad9044f62e884a7f155b834d44e95f3
nikitakrasnov <nikitakrasnov@comp.ua.exirigo-upos.pl>
2017-08-10 18:18:52 +0300

    adding account profile

--- JavaBlogAggregator/src/main/java/ua/krasnovnikita/controller/UserController.java
+++ JavaBlogAggregator/src/main/java/ua/krasnovnikita/controller/UserController.java
@@ -1,5 +1,7 @@
 package ua.krasnovnikita.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -43,6 +45,13 @@ public class UserController {
 	public String doRegister(@ModelAttribute("user") User user) {
 		userService.saveUser(user);
 		return "redirect:/register.html?success=true";
+	}
+
+	@RequestMapping("/account")
+	public String account(Model model, Principal principal) {
+		String name = principal.getName();
+		model.addAttribute("user", userService.findOneWithBlogs(name));
+		return "user-detail";
 	}
 
 }
